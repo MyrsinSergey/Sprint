bf54aff0a170a99d523fd206742a063f975d4cd7
MyrsinSergey <myrsin.s@ya.ru>
2023-08-05 01:08:40 +0400

    добавлен файл Readme.md

--- Pereval/urls.py
+++ Pereval/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pereval_app.views import SubmitData, SubmitDataDetailView
 from .yasg import urlpatterns as doc_urls
 
@@ -7,8 +7,10 @@ from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('submitData/', SubmitData.as_view()),
     path('submitData/<int:pk>/', SubmitDataDetailView.as_view()),
+
 ]
 
 urlpatterns += doc_urls
